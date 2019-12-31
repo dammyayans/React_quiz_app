82644234e13859973d2a7a787abff034853892a7
dammyayans <dammyayans560@gmail.com>
2019-12-30 19:33:00 -0800

    Create a React quiz app and publish it to Github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import "./assets/style.css";
+import quizService from "./quizService/index";
+import QuestionBox from "./components/QuestionBox";
+import Result from "./components/Result";
+export class App extends Component {
+  state = {
+    questionBank: [],
+    score: 0,
+    responses: 0
+  };
+  componentDidMount() {
+    this.getQuestion();
+  }
+  getQuestion = () => {
+    quizService().then(question => {
+      this.setState({ questionBank: question });
+    });
+  };
+  computeAnswer = (answer, correct) => {
+    if (answer === correct) {
+      this.setState({ score: this.state.score + 1 });
+    }
+    this.setState({
+      responses: this.state.responses < 5 ? this.state.responses + 1 : 5
+    });
+  };
+  playAgain = () => {
+    this.setState({ questionBank: [], score: 0, responses: 0 });
+    this.getQuestion();
+  };
+  render() {
+    return (
+      <>
+        <div className="container">
+          <div className="title">Quiz App</div>
+          {this.state.questionBank.length > 0 &&
+            this.state.responses < 5 &&
+            this.state.questionBank.map(
+              ({ question, answers, correct, questionId }) => (
+                <QuestionBox
+                  key={questionId}
+                  question={question}
+                  options={answers}
+                  selected={answer => this.computeAnswer(answer, correct)}
+                />
+              )
+            )}
+          {this.state.responses === 5 && (
+            <Result score={this.state.score} playAgain={this.playAgain} />
+          )}
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
