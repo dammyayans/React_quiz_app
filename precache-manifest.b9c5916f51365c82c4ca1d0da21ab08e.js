8f88e28745eb61a7dbdf920e612b3d78a27faa60
dammyayans <dammyayans560@gmail.com>
2019-12-30 19:52:47 -0800

    Updates

---
+++ precache-manifest.b9c5916f51365c82c4ca1d0da21ab08e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acd9f4abf26d0db1e334b701b0fede61",
+    "url": "/quiz_app/index.html"
+  },
+  {
+    "revision": "1f71c40a49acd27611e8",
+    "url": "/quiz_app/static/css/main.7f8d0d59.chunk.css"
+  },
+  {
+    "revision": "8de6e1c45f4f49ddf94c",
+    "url": "/quiz_app/static/js/2.cce057be.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/quiz_app/static/js/2.cce057be.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1f71c40a49acd27611e8",
+    "url": "/quiz_app/static/js/main.ed76de53.chunk.js"
+  },
+  {
+    "revision": "1d65d4fc352dc514c30f",
+    "url": "/quiz_app/static/js/runtime-main.bd17ba86.js"
+  }
+]);
